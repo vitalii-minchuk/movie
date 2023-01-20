7f7021199d960214de71dee97bb30a148156b044
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-01-20 09:18:11 +0200

    restsrt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,5 @@
 <template>
-  <v-row>
-    <v-btn @click="increment" prepend-icon="mdi-plus" variant="tonal">
-      Button
-    </v-btn>
-    <div>pos: {{ x }}, {{ y }}</div>
-    <v-list-item>
-      <v-list-item-content>
-        <v-list-item-title>Count: {{ count }}</v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
-  </v-row>
+  <v-row> home </v-row>
 </template>
 
-<script setup lang="ts">
-import { useMouse } from '@vueuse/core'
-import { useCounterStore } from '@/stores/counter'
-
-const { count, doubleCount, increment } = useCounterStore()
-console.log(count, doubleCount)
-const { x, y } = useMouse()
-</script>
+<script setup lang="ts"></script>
